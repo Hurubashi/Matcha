c9a0d1456acaf7e7e0d7f3c7b2173f662abd4b25
Maxym RYBAK <mrybak@e1r4p17.unit.ua>
2019-08-12 20:59:41 +0300

    tring to compose dockerfile

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,7 @@ module.exports = {
       database: process.env.DB_NAME,
       user:     process.env.DB_USER,
       password: process.env.DB_PASSWORD,
+      socketPath : process.env.DB_SOCKET,
     },
     pool: {
       min: 2,
