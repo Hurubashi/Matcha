c52845a33bad8dffe3a3170cfcd4e108362ddca0
Max Rybak <hurubashi@gmail.com>
2019-10-16 23:19:53 +0300

    npm update

--- actions/auth/EmailConfirmationAction.js
+++ actions/auth/EmailConfirmationAction.js
@@ -10,15 +10,16 @@ class EmailConfirmationAction extends BaseAction {
 	static async run(req) {
 		
 		// const match = await bcrypt.compare(req.body.password, user[0].password)
-
-		const uuid = await uuidOperation.getWhere(
-			{user_id: req.params.id})
-			.then((res) => {return res[0].uuid})
-
-		if (uuid == req.params.uuid) {
-			await UsersModel.update(req.params.id, {is_verified: 1})
-			return {result: 'Success'}
-		} else {
+		console.log(req.params)
+		try {
+			const uuid = await uuidOperation.getWhere(
+				{user_id: req.params.id})
+				.then((res) => {return res[0].uuid})
+			if (uuid == req.params.uuid) {
+				await UsersModel.update(req.params.id, {is_verified: 1})
+				return {result: 'Success'}
+			}
+		} catch {
 			return {error: 'UUID key is wrong.'}
 		}
 
