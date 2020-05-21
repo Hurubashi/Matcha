7c4b0ad75246b70f9859038ac0ded64ceb083b3f
Maxym Rybak <hurubashi@gmail.com>
2020-05-21 18:56:14 +0300

    remove build from .gitignore

---
+++ client/build/precache-manifest.e2caa98eb914fe45514083883acf3b0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756e62728ba6d979c85b988ed1ab8e34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de4067961ba7a353ee46",
+    "url": "/static/js/2.42a93b6f.chunk.js"
+  },
+  {
+    "revision": "d69551d42a4fe44f36e7b461e7ca402c",
+    "url": "/static/js/2.42a93b6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29dd3345372135ba78a0",
+    "url": "/static/js/main.9768d049.chunk.js"
+  },
+  {
+    "revision": "bd4c681accc02400a9be",
+    "url": "/static/js/runtime-main.f539de1a.js"
+  }
+]);
